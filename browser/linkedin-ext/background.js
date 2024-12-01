6bc7a179aabddaddb1a22fd7b523d62b4076be8b
aliep <p30arena@gmail.com>
2024-12-01 16:34:17 +0330

    refactoring

--- browser/linkedin-ext/background.js
+++ browser/linkedin-ext/background.js
@@ -1,13 +1,30 @@
+const execTabs = new Set();
+
+const linkedinTabKey = (tabId) => "linkedin_" + tabId;
+
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
   if (
     tab.active &&
     tab.status == "loading" &&
     tab.url.includes("linkedin.com")
   ) {
-    chrome.scripting.executeScript({
-      target: { tabId: tabId },
-      files: ["content.js"],
-    });
+    if (!execTabs.has(linkedinTabKey(tabId))) {
+      execTabs.add(linkedinTabKey(tabId));
+      chrome.scripting.executeScript({
+        target: { tabId: tabId },
+        files: ["general.js", "linkedin.js"],
+      });
+    }
+  }
+
+  if (
+    tab.active &&
+    tab.status == "complete" &&
+    tab.url.includes("linkedin.com")
+  ) {
+    if (execTabs.has(linkedinTabKey(tabId))) {
+      execTabs.delete(linkedinTabKey(tabId));
+    }
   }
 });
 
